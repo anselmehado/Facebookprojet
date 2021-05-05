fbf715dc89159db6bebb55399844dff0bc6d4c94
hado anselme <anselmehado85@yahoo.com>
2021-05-05 16:06:47 +0100

    termine

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:content)
+     params.require(:post).permit(:content,:avatar, :avatar_cache)
   end
 
 
