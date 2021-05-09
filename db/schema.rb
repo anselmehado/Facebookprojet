51fa73bd9d0f45fb4f8ebe2c76bfd55d3d96d430
hado anselme <anselmehado85@yahoo.com>
2021-05-09 15:05:23 +0100

    comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_201635) do
+ActiveRecord::Schema.define(version: 2021_05_09_134825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_201635) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "avatar"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
